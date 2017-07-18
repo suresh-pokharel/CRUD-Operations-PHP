beaac7312709ae8d89565916c31ceeeacf6de8a1
Suresh Pokharel <suresh.wrc@gmail.com>
2017-07-18 12:06:08 +0545

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
+<?php
+/*
+      Author  : Suresh Pokharel
+      Email   : suresh.wrc@gmail.com
+      GitHub  : github.com/suresh021
+      URL     : psuresh.com.np
+*/ 
+?>
+
 <?php 
 session_start();
 session_destroy();
 header("location:login.php")
-
- ?>
+?>
